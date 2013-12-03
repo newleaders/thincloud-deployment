7326726c22fe19275843e231f05f571fb97d29ce
Don Morrison <don@elskwid.net>
2013-12-02 21:36:02 -0800

    Update gem configuration
    Run `ore` against the directory and merge in various bits and pieces
    that are more in line with current gem practices, including:
    
    * documentation
    * rake test runner
    * gem dependencies
    * rubygems-tasks
    * gemspec configuration
    * add Changelog

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+### 1.0.1 / 2013-03-31
+
+  * Release version (bousquet)
+
+### 1.0.0 - ancient history ...
--- test/thincloud-deployment_test.rb
+++ test/thincloud-deployment_test.rb
@@ -1,4 +1,4 @@
-require "minitest_helper"
+require "helper"
 
 describe Thincloud::Deployment do
   it { Thincloud::Deployment.must_be_kind_of Module }
